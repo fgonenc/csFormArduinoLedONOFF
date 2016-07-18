94c9299b094684c18eb986e03d2ff7e084a39022
fgonenc <francois.gonenc@hotmail.fr>
2016-07-18 12:44:46 +0200

    Add project files.

---
+++ csFormArduinoLedONOFF/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace csFormArduinoLedONOFF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnSerialConnect = new System.Windows.Forms.Button();
+            this.lbSerialports = new System.Windows.Forms.ListBox();
+            this.gbConnection = new System.Windows.Forms.GroupBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gbConnection.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(177, 297);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(97, 26);
+            this.btnOn.TabIndex = 0;
+            this.btnOn.Text = "LED ON";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(177, 329);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(97, 26);
+            this.btnOff.TabIndex = 1;
+            this.btnOff.Text = "LED OFF";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // btnSerialConnect
+            // 
+            this.btnSerialConnect.Location = new System.Drawing.Point(177, 244);
+            this.btnSerialConnect.Name = "btnSerialConnect";
+            this.btnSerialConnect.Size = new System.Drawing.Size(97, 26);
+            this.btnSerialConnect.TabIndex = 2;
+            this.btnSerialConnect.Text = "#";
+            this.btnSerialConnect.UseVisualStyleBackColor = true;
+            this.btnSerialConnect.Click += new System.EventHandler(this.btnSerialConnect_Click);
+            // 
+            // lbSerialports
+            // 
+            this.lbSerialports.FormattingEnabled = true;
+            this.lbSerialports.Location = new System.Drawing.Point(17, 29);
+            this.lbSerialports.Name = "lbSerialports";
+            this.lbSerialports.Size = new System.Drawing.Size(108, 82);
+            this.lbSerialports.TabIndex = 3;
+            // 
+            // gbConnection
+            // 
+            this.gbConnection.Controls.Add(this.lbSerialports);
+            this.gbConnection.Location = new System.Drawing.Point(12, 233);
+            this.gbConnection.Name = "gbConnection";
+            this.gbConnection.Size = new System.Drawing.Size(141, 122);
+            this.gbConnection.TabIndex = 4;
+            this.gbConnection.TabStop = false;
+            this.gbConnection.Text = "Available Serial Ports:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMessage.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(8, 9);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(278, 37);
+            this.lblMessage.TabIndex = 5;
+            this.lblMessage.Text = "WELCOME";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::csFormArduinoLedONOFF.Properties.Resources.arduino;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 178);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(298, 379);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.gbConnection);
+            this.Controls.Add(this.btnSerialConnect);
+            this.Name = "Form1";
+            this.Text = "ARDUINO";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbConnection.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnSerialConnect;
+        private System.Windows.Forms.ListBox lbSerialports;
+        private System.Windows.Forms.GroupBox gbConnection;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ csFormArduinoLedONOFF/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csFormArduinoLedONOFF
+{
+/* This project needs Arduino UNO Board 
+ * and .ino code already Build/Uploaded.
+ * Application: LED ON/OFF with Arduino
+ * using C Sharp Forms Application. Tested. OK.
+ * */
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                lbSerialports.Items.Add(port);
+            }
+
+            lblMessage.Text =  "Arduino LED ON/OFF";
+
+            if (!serialPort1.IsOpen)
+            {
+                btnSerialConnect.Text = "CONNECT";
+                EtatDeconnecte();
+            }
+            else if (serialPort1.IsOpen)
+                {
+                btnSerialConnect.Text = "DISCONNECT";
+                EtatConnecte();
+            }
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                btnOn.Enabled = false;
+                btnOff.Enabled = true;
+                serialPort1.Write("1");
+                lblMessage.Text = "LED ON !";
+            }
+            
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                btnOff.Enabled = false;
+                btnOn.Enabled = true;
+                byte[] valueToSend = System.Text.Encoding.UTF8.GetBytes("0");
+                serialPort1.Write(valueToSend,0,valueToSend.Length);
+
+                lblMessage.Text = "LED OFF !";
+
+            }
+        }
+
+        private void btnSerialConnect_Click(object sender, EventArgs e)
+        {
+
+            string buttontext = ((Control)sender).Text;
+            if (buttontext.StartsWith("CON"))
+            {
+
+
+                if (serialPort1.IsOpen)
+                {
+                    lblMessage.Text = "Port déjà ouverte !";
+                    return;
+                }
+
+                if (lbSerialports.Items.Count > 0)
+                {
+                    lbSerialports.SelectedIndex = 0;
+                    this.Refresh();
+                }
+                else
+                {
+                    lblMessage.Text = "COM Port liste vide!";
+                    EtatDeconnecte();
+                    return;
+                }
+
+                try
+                {
+                    serialPort1.PortName = lbSerialports.SelectedItem.ToString();
+                    serialPort1.BaudRate = 9600;
+                    // if serial port is not open
+                    if (!serialPort1.IsOpen)
+                    {
+                        // then open
+                        serialPort1.Open();
+                        Refresh();
+                        // if open
+                        if (serialPort1.IsOpen)
+                        {
+                            lblMessage.Text = "Connecté COM5.";
+
+                            EtatConnecte();
+
+                        }
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Port déjà ouverte.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + ex.StackTrace);
+                }
+            }
+            else if (buttontext.StartsWith("DIS"))
+            {
+                if (serialPort1.IsOpen)
+                {
+                    // lancer delegate fermeture connection
+                    DelegateFermeture df = new DelegateFermeture(FermerCom);
+                    df();
+                    // arranger visuellement pour etat visuelle
+                    EtatDeconnecte();
+                }
+            }
+        }
+
+        public void FermerCom()
+        {
+            serialPort1.Close();
+
+            if (!serialPort1.IsOpen)
+            {
+                // display closed
+                lblMessage.Text = "Porte fermé.";
+            }
+        }
+
+        public delegate void DelegateFermeture();
+
+        public void EtatDeconnecte()
+        {
+            btnOn.Visible = false;
+            btnOff.Visible = false;
+            if (!serialPort1.IsOpen)
+            {
+                btnSerialConnect.Text = "CONNECT";
+                //EtatDeconnecte();
+            }
+            else if (serialPort1.IsOpen)
+            {
+                btnSerialConnect.Text = "DISCONNECT";
+                //EtatConnecte();
+            }
+        }
+        public void EtatConnecte()
+        {
+            btnOn.Visible = true;
+            btnOff.Visible = true;
+            if (!serialPort1.IsOpen)
+            {
+                btnSerialConnect.Text = "CONNECT";
+                //EtatDeconnecte();
+            }
+            else if (serialPort1.IsOpen)
+            {
+                btnSerialConnect.Text = "DISCONNECT";
+                //EtatConnecte();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                // lancer delegate fermeture connection
+                DelegateFermeture df = new DelegateFermeture(FermerCom);
+                df();
+                // arranger visuellement pour etat visuelle
+                EtatDeconnecte();
+            }
+        }
+
+    }
+
+}
